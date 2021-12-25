cc104d75aad378f180bbbf8a613c8503e4920880
AndreaAguado <andrea95am@gmail.com>
2021-12-25 20:16:21 +0100

    add tests done in TDD of findMultiples

--- src/findMultiples.js
+++ src/findMultiples.js
@@ -9,6 +9,30 @@ Examples:
 findMultiples(2, 6) // return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
 */
 
-function findMultiples(base, limit) {}
+function findMultiples(base, limit) {
+    if (base < 1 || limit < 1) {
+        return "Base and limit must be larger than 0";
+    }
+    if (base === undefined || limit === undefined) {
+        return "Base and limit must be natural number larger than 0";
+    }
+    if (limit < base) {
+        return "Limit cannot be larger than base";
+    }
+    let solutionArray = [];
+    let objectToReturn = {};
+    objectToReturn.solutionArray = solutionArray;
+    for (let index = 1; index < limit; index++) {
+        let aux = base * index;
+        if (aux <= limit) {
+            solutionArray.push(aux);
+        }
+        else if (aux > limit) {
+            return objectToReturn.solutionArray;
+        }
+    }
+
+
+}
 
 module.exports = findMultiples;
--- test/findMultiples.test.js
+++ test/findMultiples.test.js
@@ -1,3 +1,26 @@
 const findMultiples = require("../src/findMultiples");
 
 // write test cases to ensure findMultiples() works as expected
+test("findMultiples() returns 'Base and limit must be natural number larger than 0'", () => {
+    expect(findMultiples()).toEqual("Base and limit must be natural number larger than 0");
+})
+
+test("findMultiples(-2,6) returns 'Base and limit must be larger than 0'", () => {
+    expect(findMultiples(-2, 6)).toEqual("Base and limit must be larger than 0");
+})
+
+test("findMultiples(2, 6) returns [2, 4, 6]", () => {
+    expect(findMultiples(2, 6)).toEqual([2, 4, 6]);
+})
+
+test("findMultiples(3, 12) returns [3, 6, 9, 12]", () => {
+    expect(findMultiples(3, 12)).toEqual([3, 6, 9, 12]);
+})
+
+test("findMultiples(12, 3) returns 'Limit cannot be larger than base'", () => {
+    expect(findMultiples(12, 3)).toEqual("Limit cannot be larger than base");
+})
+
+test("findMultiples('2', '6') returns [2, 4, 6]", () => {
+    expect(findMultiples("2", "6")).toEqual([2, 4, 6]);
+})
